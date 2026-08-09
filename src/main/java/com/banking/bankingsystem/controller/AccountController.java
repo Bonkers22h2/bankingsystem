9797453e230b8f760d073039eea2b4ce0c196531
Bonkers <bonkcat78@gmail.com>
2026-08-09 17:57:29 +0800

    FEAT: trasnfer, withdraw, deposit methods are compelte

--- src/main/java/com/banking/bankingsystem/controller/AccountController.java
+++ src/main/java/com/banking/bankingsystem/controller/AccountController.java
@@ -1,12 +1,15 @@
 package com.banking.bankingsystem.controller;
 
 import com.banking.bankingsystem.dto.CreateAccountRequest;
+import com.banking.bankingsystem.dto.DepositRequest;
 import com.banking.bankingsystem.model.Account;
 import com.banking.bankingsystem.service.AccountService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/accounts")
@@ -29,6 +32,24 @@ public class AccountController {
     public ResponseEntity<Account> getAccount(@PathVariable Long id) {
         Account account = accountService.getAccount(id);
         return ResponseEntity.ok(account);
+    }
+
+    @PostMapping("/{id}/withdraw")
+    public ResponseEntity<Void> withdraw(@PathVariable Long id, @Valid @RequestBody DepositRequest request) {
+        accountService.withdraw(id, request.getAmount());
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/{id}/deposit")
+    public ResponseEntity<Void> deposit(@PathVariable Long id, @Valid @RequestBody DepositRequest request) {
+        accountService.deposit(id, request.getAmount());
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/{fromAccountId}/transfer/{toAccountId}")
+    public ResponseEntity<Void> transfer(@PathVariable Long fromAccountId, @PathVariable Long toAccountId, @Valid @RequestBody DepositRequest request){
+        accountService.transfer(fromAccountId, toAccountId, request.getAmount());
+        return ResponseEntity.ok().build();
     }
 
 }
